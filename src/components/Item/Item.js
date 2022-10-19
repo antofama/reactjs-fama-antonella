450f8682a66fd236171cfa37500c26b8f28126c7
AntoFamagit <antoefama@gmail.com>
2022-10-19 20:44:38 -0300

    contexto

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,6 @@
 import './Item.css';
 import { Link } from 'react-router-dom';
-import { useContext } from 'react';
-import { CartContext } from '../../Context/CartContext';
+
 
 const Item = ({info}) =>{
     
